b3e74e4f1aa6662925224d4586ec26062ad03734
Hyeonje09 <jhj37299455@gmail.com>
2024-02-06 13:41:10 +0900

    초기 커밋.

---
+++ ShooTing_Game/main.cpp
@@ -0,0 +1,60 @@
+#pragma warning(disable:4996)
+
+#include <windows.h>
+#include <conio.h>
+#include <stdio.h>
+
+// 콘솔 창 세팅
+void Setting_Console() {
+	system("Flight Game"); // 콘솔창 이름
+	system("mode con:cols=60 lines=60"); // 콘솔창 크기
+
+	CONSOLE_CURSOR_INFO ConsoleCursor;
+	ConsoleCursor.bVisible = 0;
+	ConsoleCursor.dwSize = 1;
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorInfo(consoleHandle, &ConsoleCursor);
+}
+
+void textcolor(int color_number) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+}
+
+void gotoxy(int x, int y) {
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD Cur;
+	Cur.X = x;
+	Cur.Y = y;
+	SetConsoleCursorPosition(consoleHandle, Cur);
+}
+
+int main() {
+	Setting_Console();
+	int x = 0, y = 0;
+	int yMax = 60, xMax = 60;
+	while (1) {
+		gotoxy(x, y);
+		printf("*");
+
+		//GetAsyncKeyState : 키 입력 함수
+		if (GetAsyncKeyState(VK_UP) & 0x8000) {
+			if (y > 0)
+				y--;
+		}
+		if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+			if (y < yMax - 1)
+				y++;
+		}
+		if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
+			if (x > 0)
+				x -= 2;
+		}
+		if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
+			if (x < xMax - 2)
+				x += 2;
+		}
+
+		Sleep(50);
+	}
+	return 0;
+}
